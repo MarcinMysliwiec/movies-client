fc641247c4059eb4220836c98d2cda068d06d0bc
Marcin Mysliwiec <marcin.mysliw@gmail.com>
2022-02-08 11:14:28 +0100

    Filter Movies css

--- src/components/FilterMovies.js
+++ src/components/FilterMovies.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import './FilterMovies.css'
+import { ReactComponent as TrendingSvg } from '../assets/images/svgs/trending.svg';
+import { ReactComponent as StarSvg } from '../assets/images/svgs/star.svg';
+import { ReactComponent as PlaySvg } from '../assets/images/svgs/play.svg';
+
 
 function FilterMovies({ showMoviesOnBtn }) {
   return (
     <div className='navlink-container'>
       <button onClick={showMoviesOnBtn} className='popular' id='filter-btn'>
-        <img src='/svgs/trending.svg' className='popular' alt='' />
+        <TrendingSvg className='popular' alt='popular'/>
         Popular
       </button>
       <button onClick={showMoviesOnBtn} className='top_rated' id='filter-btn'>
-        <img src='/svgs/star.svg' className='top_rated' alt='' />
+        <StarSvg className='top_rated' alt='top rated'/>
         Top <br />
         Rated
       </button>
       <button onClick={showMoviesOnBtn} className='now_playing' id='filter-btn'>
-        <img src='/svgs/play.svg' className='now_playing' alt='' />
+        <PlaySvg className='now_playing' alt='now playing'/>
         Now <br />
         Playing
       </button>
