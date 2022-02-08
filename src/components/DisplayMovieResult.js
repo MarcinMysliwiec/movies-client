2ef4098246723dbe6efc8f6e539fa59f3c4339e9
Marcin Mysliwiec <marcin.mysliw@gmail.com>
2022-02-08 11:05:42 +0100

    Movie result css

--- src/components/DisplayMovieResult.js
+++ src/components/DisplayMovieResult.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import { fadeIn } from 'react-animations'
+import Radium, {StyleRoot} from 'radium';
+import './DisplayMovieResult.css'
+
+const styles = {
+  fadeIn: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(fadeIn, 'fadeIn')
+  }
+}
 
 function DisplayMovieResult({ result, getMovieID }) {
   const isPosterUrlvalid = result.poster_path !== null ? `https://image.tmdb.org/t/p/w220_and_h330_face/${result.poster_path}` : '/svgs/TMDB_LOGO.svg';
 
   return (
-    <div className='card' onClick={getMovieID} id={result.id}>
-      <img src={`${isPosterUrlvalid}`} className='card-poster' alt={`${result.title} Poster`}></img>
-      <div className='card-release_date'>{result.release_date && result.release_date.split('-')[0]}</div>
-      <div className='card-title'>
-        <p>{result.title}</p>
-      </div>
-    </div>
+    <StyleRoot className='card' onClick={getMovieID} id={result.id} style={styles.fadeIn}>
+        <img className='card-poster' src={`${isPosterUrlvalid}`} alt={`${result.title} Poster`}/>
+        <div className='card-release_date'>{result.release_date && result.release_date.split('-')[0]}</div>
+        <div className='card-title'>
+          <p>{result.title}</p>
+        </div>
+    </StyleRoot>
   );
 }
 
